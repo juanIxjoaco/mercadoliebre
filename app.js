ab73118dbbb6aa8f850944da97938d9a3445a4aa
juanIxjoaco <juaniingelmo@gmail.com>
2022-05-28 16:10:35 -0300

    empezando con login y register

--- app.js
+++ app.js
@@ -12,3 +12,11 @@ app.listen(port, ()=>{
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
 });
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
